4aee4485c9648eac78add363ace77a46a697639c
ehernandezp <edgardhernandezp@gmail.com>
2024-03-06 07:42:32 -0300

    Generated a new project using Spring Initializer

---
+++ src/main/java/com/dreamseeker/userinfoservice/UserInfoServiceConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.dreamseeker.userinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInfoServiceConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserInfoServiceConsumerApplication.class, args);
+	}
+
+}
